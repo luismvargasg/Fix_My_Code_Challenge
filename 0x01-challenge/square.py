038fa6ae1b067dfb249d36ac7b521a18c15eb2d1
luismvargasg <1610@holbertonschool.com>
2020-09-29 20:32:58 +0000

    adding type and value check

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Module to define a class Square
+"""
+Module to define a class Square
 """
 
 
-class square():
+class Square():
     """Defining the class Square
 
     Returns:
@@ -16,6 +17,36 @@ class square():
         """Constructor for the class Square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for the private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter for the private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     def area_of_my_square(self):
         """Area of the square"""
@@ -30,7 +61,7 @@ class square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = square(width=12, height=12)
+    s = Square(width=9, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
