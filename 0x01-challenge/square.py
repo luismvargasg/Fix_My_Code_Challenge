30edad6cdb02d94d3c227df8b2048085d25b6ff2
luismvargasg <1610@holbertonschool.com>
2020-09-29 20:38:00 +0000

    changing camel case def

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,13 +10,14 @@ class Square():
     Returns:
         Nothing.
     """
+
     width = 0
     height = 0
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, width, height):
         """Constructor for the class Square"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -52,7 +53,7 @@ class Square():
         """Area of the square"""
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def Permiter_of_my_square(self):
         """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -64,4 +65,4 @@ if __name__ == "__main__":
     s = Square(width=9, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.Permiter_of_my_square())
